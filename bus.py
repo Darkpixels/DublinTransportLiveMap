6ac00ae2d05d454173a0321888c657b44acac865
Darkpixels <ngumangueira@gmail.com>
2016-07-26 21:44:33 +0100

    Create bus.py

---
+++ bus.py
@@ -0,0 +1,2 @@
+#!/bin/bash/python
+cols_text = 'StopPointName,StopID,Towards,StopPointIndicator,StopPointState,Latitude,Longitude,VisitNumber,LineID,LineName,DirectionID,DestinationName,VehicleID,TripID,RegistrationNumber,EstimatedTime,ExpireTime'
